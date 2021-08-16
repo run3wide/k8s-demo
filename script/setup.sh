e8be50797ba69693a16361916d657b7f7d5a188d
Paul Robson <paul@run3wide.com>
2021-08-16 19:53:32 -0400

    Automatically start grafana dashboard

--- script/setup.sh
+++ script/setup.sh
@@ -16,11 +16,12 @@ install_istio_addons() {
   kubectl apply -f "$ISTIO_DIRECTORY"/samples/addons/grafana.yaml
 }
 
-start_dashboard() {
+start_dashboards() {
   x-terminal-emulator -e istioctl dashboard kiali
+  x-terminal-emulator -e istioctl dashboard grafana
 }
 
 install_istio
 mark_pods_for_istio_injection
 install_istio_addons
-start_dashboard
+start_dashboards
