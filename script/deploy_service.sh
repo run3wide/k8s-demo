6665c43f8f01ea1e35ca10d552fc92e72131f2f0
Paul Robson <paul@run3wide.com>
2021-08-16 19:35:10 -0400

    Fail deployment when SERVICE_NAME not set

--- script/deploy_service.sh
+++ script/deploy_service.sh
@@ -1,17 +1,17 @@
 #!/bin/sh
 
 build_docker_image() {
-  cd ../"$SERVICE_NAME" || exit
+  cd ../"$SERVICE_NAME" || exit 1
   ./gradlew bootBuildImage
 }
 
 load_docker_image_into_kubernetes_cluster() {
-    kind load docker-image docker.io/run3wide/"$SERVICE_NAME":latest docker.io/run3wide/"$SERVICE_NAME":latest --name dev-cluster
+    kind load docker-image run3wide/"$SERVICE_NAME":latest run3wide/"$SERVICE_NAME":latest --name dev-cluster
 }
 
 deploy_service() {
-  kubectl replace --force -f ../"$SERVICE_NAME"/infrastructure/resources.yml
-  kubectl replace --force -f ../"$SERVICE_NAME"/infrastructure/virtual_service.yml
+  kubectl replace --force -f "infrastructure/resources.yml"
+  kubectl replace --force -f "infrastructure/virtual_service.yml"
 }
 
 test_app_endpoint() {
@@ -19,15 +19,21 @@ test_app_endpoint() {
   INGRESS_PORT=$(kubectl -n istio-system get service istio-ingressgateway -o jsonpath='{.spec.ports[?(@.name=="http2")].nodePort}')
 
   APP_URL=$INGRESS_HOST:$INGRESS_PORT
-  echo "$APP_URL"
+  echo "Kubernetes Ingress: $APP_URL"
 
   sleep 10
   curl -HHost:user-api.run3wide.com "$APP_URL"/actuator/health
   echo
 }
 
+set e
 SERVICE_NAME=$1
 CURRENT_DIRECTORY=$PWD
+
+if [ -z "$SERVICE_NAME" ]; then
+  echo "SERVICE_NAME not set"
+    exit 1
+fi
 
 build_docker_image
 load_docker_image_into_kubernetes_cluster
